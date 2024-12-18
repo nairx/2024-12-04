7ed493796870a270b03c2bb7b32fc06327a17e33
nairx <nairxprv@gmail.com>
2024-12-18 10:39:37 +0530

    added files

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let students = [
-    { name: "Revanth", user: "revanthyellapragada17" },
+  {name:'Madhu Siripireddy',user:'madhu494'},
+  {name:'Hareesh Madinedi',user:'madinedihareesh'},
+  {name:'Rinku Jacob',user:'rinkujustin10'},
+  {name:'Yaswanth Reddy',user:'yaswanthsivaa'},
+  {name:'Rashmi',user:'Rashmiit'},
+  {name:'Supriya Kukunuru',user:'SUPRIYAKUKUNURU'},
+  {name:'Tarun Babu Marriboyina',user:'Tarun6987'},
+  {name:'PATTI RUKMAN GADA',user:'pattirukmangada'},
+  
   ];
 
 
